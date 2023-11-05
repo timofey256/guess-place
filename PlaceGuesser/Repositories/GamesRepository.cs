38f66ec5c1f22128a348ed21a9e0f2d772b19313
schetilt <shetilin.timofey@gmail.com>
2023-11-06 00:12:18 +0100

    Changed games repository to use Redis

--- PlaceGuesser/Repositories/GamesRepository.cs
+++ PlaceGuesser/Repositories/GamesRepository.cs
@@ -1,48 +1,69 @@
 using PlaceGuesser.Models;
+using NRedisStack;
+using NRedisStack.RedisStackCommands;
+using StackExchange.Redis;
+using System.Text.Json;
 
 namespace PlaceGuesser.Repositories;
 
 public class GamesRepository
 {
-    private int _currentIndex = 0;
-    private Dictionary<int, Game> _games = new Dictionary<int, Game>();
+    private static readonly ConnectionMultiplexer Redis = ConnectionMultiplexer.Connect("localhost");
+    private static int _currentIndex = 0;
+    private IDatabase _db = Redis.GetDatabase();
 
     public GamesRepository() { }
-    
+        
     public int Add(GamePreferences preferences)
     {
         var newGame = new Game(preferences);
-        _games.Add(_currentIndex, newGame);
-        _currentIndex++;
+        string serializedGame = JsonSerializer.Serialize(newGame);
+        _db.StringSet(_currentIndex.ToString(), serializedGame);
+        _currentIndex += 1;
         return _currentIndex-1;
+    }
+
+    public Game? Get(int gameId)
+    {
+        string? serializedGame = _db.StringGet(gameId.ToString());
+        return serializedGame is not null ? JsonSerializer.Deserialize<Game>(serializedGame) : null;
     }
 
     public bool IsGameOver(int gameId)
     {
-        var game = _games[gameId];
-        return game.Rounds.Count == game.NumberOfRounds;
+        string? serializedGame = _db.StringGet(gameId.ToString());
+        if (serializedGame == null) return true;
+        
+        var game = JsonSerializer.Deserialize<Game>(serializedGame);
+        return game?.NumberOfRounds == game?.Rounds.Count;
     }
 
     public void CreateNewRound(int gameId, Video video)
     {
-        var game = _games[gameId];
-        game.AddRound(video);
+        throw new NotImplementedException();
     }
 
     public GamePreferences GetPreferences(int gameId)
     {
-        var game = _games[gameId];
-        return new GamePreferences() {Continent = game.Continent, VideoType = game.VideoType, NumberOfRounds = game.NumberOfRounds};
+        string? serializedGame = _db.StringGet(gameId.ToString());
+        if (serializedGame == null) throw new ArgumentException("Unexpected gameID. There's no such ID in DB");
+        
+        var game = JsonSerializer.Deserialize<Game>(serializedGame);
+        return new GamePreferences() {Continent = game.Continent, NumberOfRounds = game.NumberOfRounds, VideoType = game.VideoType};
     }
 
     public Coordinates GetCoordsOfLastRound(int gameId)
     {
-        var game = _games[gameId];
+        string? serializedGame = _db.StringGet(gameId.ToString());
+        if (serializedGame == null) throw new ArgumentException("Unexpected gameID. There's no such ID in DB");
+        
+        var game = JsonSerializer.Deserialize<Game>(serializedGame);
         return game.Rounds[^1].Coordinates;
     }
 
     public bool Contains(int gameId)
     {
-        return _games.ContainsKey(gameId);
+        string? serializedGame = _db.StringGet(gameId.ToString());
+        return serializedGame is not null;
     }
 }
