be3f2a480e68e0a2237e53357adb632493c24ac9
schetilt <shetilin.timofey@gmail.com>
2023-11-09 00:27:52 +0100

    Modified new exception types

--- PlaceGuesser/Exceptions/AppInvalidIdException.cs
+++ PlaceGuesser/Exceptions/AppInvalidIdException.cs
@@ -2,5 +2,11 @@ namespace PlaceGuesser.Exceptions;
 
 public class AppInvalidIdException : Exception
 {
-    public AppInvalidIdException(string message) : base(message) {}
+    public readonly int Id;
+    private const string _messageTemplate = "Unexpected ID of a game. There's no such ID={0} in the database.";
+
+    public AppInvalidIdException(int id) : base(string.Format(_messageTemplate, id))
+    {
+        Id = id;
+    }
 }
--- PlaceGuesser/Exceptions/AppRedisIsDownException.cs
+++ PlaceGuesser/Exceptions/AppRedisIsDownException.cs
@@ -1,6 +1,7 @@
 namespace PlaceGuesser.Exceptions;
 
-public class AppRedisIsDownException
+public class AppRedisIsDownException : Exception
 {
-    
+    private const string _message = "Redis server is not started. Whole message : {0}";
+    public AppRedisIsDownException(string wholeMessage) : base(string.Format(_message, wholeMessage)) {}
 }
